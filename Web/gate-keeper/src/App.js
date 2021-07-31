937836fc8184fac227107709084e6e09c515c9c6
AchinthaHarshamal <achinthafharshamal@gmail.com>
2021-07-31 19:35:25 +0530

    Implemented the basic Home page "Banne + download"

--- Web/gate-keeper/src/App.js
+++ Web/gate-keeper/src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/layout/Navbar";
 import {BrowserRouter} from 'react-router-dom';
+import Home from "./components/home/Home";
 
 function App() {
   return (
 
     <BrowserRouter>
       <Navbar/>
+      <Home />
     </BrowserRouter>
     
   );
--- Web/gate-keeper/src/components/layout/Navbar.js
+++ Web/gate-keeper/src/components/layout/Navbar.js
@@ -7,9 +7,9 @@ import SignedOutLinks from './SignedOutLinks';
 const Navbar =()=> {
     return (
         <div>
-            <nav className="nav-wraper  indigo darken-4">
-                <div className="container">
-                    <Link to='/' className="brand-logo">RGK</Link>
+            <nav className="nav-wraper  indigo darken-2">
+                <div>
+                    <Link to='/' className="brand-logo center">RGK</Link>
                     <SignedInLinks />
                     <SignedOutLinks />
                 </div>
