1b03cc567f55f560069b3a25588e686aeb7bb722
AchinthaHarshamal <achinthafharshamal@gmail.com>
2021-08-05 00:50:16 +0530

    Subscribed the store by the dashbord. Added some dumy data

--- Web/gate-keeper/src/components/dashbord/Dashbord.js
+++ Web/gate-keeper/src/components/dashbord/Dashbord.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import Hlist from './Hlist';
+class Dashbord extends Component {
 
-export class Dashbord extends Component {
     render() {
+        //console.log(this.props)
+        const {messages} = this.props;
+        //console.log(messages)
         return (
             <div className="container">
-                <h1>Hello this is from some one</h1>                
+                <h1>Hello Dashboard</h1> 
+                <Hlist messages={messages}/>
+
             </div>
         )
     }
 }
 
-export default Dashbord;
+const mapStateToProps =(state) => {
+    return {
+        messages: state.dboard.messages
+    }
+}
+
+export default connect(mapStateToProps)(Dashbord);
 
