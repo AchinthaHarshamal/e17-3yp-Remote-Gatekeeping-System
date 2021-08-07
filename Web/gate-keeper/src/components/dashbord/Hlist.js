0b1e8442a8b8ead375aca92d3e94186073a4e0d7
AchinthaHarshamal <achinthafharshamal@gmail.com>
2021-08-07 13:26:32 +0530

    added the card to dash board

--- Web/gate-keeper/src/components/dashbord/Hlist.js
+++ Web/gate-keeper/src/components/dashbord/Hlist.js
@@ -1,15 +1,25 @@
 import React from 'react'
 
 const  Hlist  = ({messages}) =>  {
+
+    const cards = messages.map( (msg) =>{
+            return (
+                <div class="col s12 l3">
+                    <div class="card z-depth-0 deep-purple lighten-2zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz">
+                        <div class="card-content white-text ">  
+                            <span class="card-title">{msg.dataType}</span>
+                            <p>From : {msg.from}</p>
+                            <p>To: {msg.to} </p>
+                            <p>Date : {msg.time}</p>
+                        </div>
+                    </div>
+                </div>
+            )})
+    
    
     return (
-        <div>
-             <h1>Hello how are your</h1>
-            { messages.map( (msg) => {
-                return (
-                    <h5 key={msg.id}>from : {msg.from} to : {msg.to} type : {msg.dataType}</h5>
-                )
-            })}
+        <div className ="row">
+            {cards}
         </div>
     )
 }
--- Web/gate-keeper/src/store/reducers/dashboardReducer.js
+++ Web/gate-keeper/src/store/reducers/dashboardReducer.js
@@ -1,9 +1,13 @@
 const initState = {
     messages:[
-        {id: '1' , from : 'Sampath' , to:'Node' , dataType :'image'},
-        {id: '2' , from : 'Sam' , to:'Node1' , dataType :'image'},
-        {id: '3' , from : 'Janaka' , to:'Node2' , dataType :'Voice'},
-        {id: '4' , from : 'Jkal' , to:'Node3' , dataType :'Voice'}
+        {id: '1' , from : 'Sampath' , to:'Node' , dataType :'image' , time : '22/45/2021'},
+        {id: '2' , from : 'Sam' , to:'Node1' , dataType :'image' , time : '22/45/2021'},
+        {id: '3' , from : 'Janaka' , to:'Node2' , dataType :'Voice' , time : '22/45/2021'},
+        {id: '4' , from : 'Jkal' , to:'Node3' , dataType :'Voice' , time : '22/45/2021'},
+        {id: '5' , from : 'Sam' , to:'Node1' , dataType :'image' , time : '22/45/2021'},
+        {id: '6' , from : 'Janaka' , to:'Node2' , dataType :'Voice' , time : '22/45/2021'},
+        {id: '7' , from : 'Jkal' , to:'Node3' , dataType :'Voice' , time : '22/45/2021'}
+
 
     ]
 }
