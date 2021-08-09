968b546eb81a4ddad1be1bc02f531a2801b06f35
AchinthaHarshamal <achinthafharshamal@gmail.com>
2021-08-09 22:40:46 +0530

    Signout sucessfully

--- Web/gate-keeper/src/components/layout/SignedInLinks.js
+++ Web/gate-keeper/src/components/layout/SignedInLinks.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
+import { signOut } from '../../store/actions/authActions';
 
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
+    
     return (
         <ul className="right">
             <li><NavLink to="/dashboard">Dashboard</NavLink></li>
-            <li><NavLink to="/">Log Out</NavLink></li>
+            <li><a onClick = {props.signOut}>Log Out</a></li>
             <li><NavLink to="/" className="btn btn-floating yello darken-5">AH</NavLink></li>
         </ul>
 
     )
 }
 
-export default SignedInLinks
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut :  () => dispatch(signOut()) 
+    }
+}
+
+export default connect(null , mapDispatchToProps)(SignedInLinks)
