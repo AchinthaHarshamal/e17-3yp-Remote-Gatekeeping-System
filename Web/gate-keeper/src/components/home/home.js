8883bc6da9270638e0ec819286ee0585902f8795
AchinthaHarshamal <achinthafharshamal@gmail.com>
2021-09-16 19:48:31 +0530

    home page CI = ''

--- Web/gate-keeper/src/components/home/home.js
+++ Web/gate-keeper/src/components/home/home.js
@@ -15,7 +15,7 @@ const  Home = () => {
         <div className="home">
             <div className="row">
                 <div className="col  s12 m7 offset-m1 grey lighten-2">
-                    Hello
+                    Hello Home
                 </div>
                 
                 <div className="col  s12 m3 grey lighten-3 ">
