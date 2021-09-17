3dd3be1e536f45081c8aa09bf73e054823c3d52a
IsharaNawarathna <e17219@eng.pdn.ac.lk>
2021-09-17 20:11:38 +0530

    Optimization to the UI of the Mobile

--- Mobile/App.js
+++ Mobile/App.js
@@ -6,13 +6,6 @@ import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 
 import Navigation from "./navigation/Navigation";
-import WelcomePage from "./screens/WelcomePage";
-import LoginScreen from "./screens/LoginScreen";
-import PreviousEvents from "./screens/PreviousEvents";
-import PreviousEventDetails from "./screens/PreviousEventDetails";
-import ActiveEventScreen from "./screens/ActiveEventScreen";
-import CloseActiveEventScreen from "./screens/CloseActiveEventScreen";
-import selectIdContext from "./contextAPI/selectId";
 import { event, set } from "react-native-reanimated";
 
 const fetchFonts = () => {
@@ -24,12 +17,6 @@ const fetchFonts = () => {
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
-  const [authorized, setAuthorized] = useState(false);
-  const [prevEvents, setPrevEvents] = useState(false);
-  const [preEventDetails, setPrevEventDetails] = useState(false);
-  const [activeEvent, setActiveEvent] = useState(false);
-  const [closeEvent, setCloseEvent] = useState(false);
-  const [eventId, setEventId] = useState(false);
 
   if (!fontLoaded) {
     return (
@@ -40,99 +27,6 @@ export default function App() {
       />
     );
   }
-  const handleLogIn = () => {
-    setAuthorized(true);
-  };
-  const hangleLogOut = () => {
-    setAuthorized(false);
-  };
-  const handlePreveEvent = () => {
-    setPrevEvents(true);
-    setAuthorized(false);
-  };
-  const handlePreveEventBack = () => {
-    setPrevEvents(false);
-    setAuthorized(true);
-  };
-  const handlePreveEventDetails = (id) => {
-    setEventId(id);
-    setPrevEvents(false);
-    setPrevEventDetails(true);
-  };
-  const handlePreveEventDetailsBack = () => {
-    setPrevEvents(true);
-    setPrevEventDetails(false);
-  };
 
-  const handleActiveEvent = () => {
-    setActiveEvent(true);
-    setAuthorized(false);
-  };
-
-  const handleActiveEventBack = () => {
-    setActiveEvent(false);
-    setAuthorized(true);
-  };
-
-  const handleCloseEvent = () => {
-    setCloseEvent(true);
-    setActiveEvent(false);
-  };
-
-  const handleCloseEventBack = () => {
-    setCloseEvent(false);
-    setActiveEvent(true);
-  };
-
-  const handleClosing = () => {
-    setCloseEvent(false);
-    setAuthorized(true);
-  };
-
-  if (authorized) {
-    return (
-      <WelcomePage
-        onPress={hangleLogOut}
-        clickPrev={handlePreveEvent}
-        clickActive={handleActiveEvent}
-      ></WelcomePage>
-    );
-  }
-
-  if (prevEvents) {
-    return (
-      <PreviousEvents
-        onBack={handlePreveEventBack}
-        onPress={handlePreveEventDetails}
-      ></PreviousEvents>
-    );
-  }
-  if (preEventDetails) {
-    return (
-      <PreviousEventDetails
-        onBack={handlePreveEventDetailsBack}
-        id={eventId}
-      ></PreviousEventDetails>
-    );
-  }
-
-  if (activeEvent) {
-    return (
-      <ActiveEventScreen
-        onBack={handleActiveEventBack}
-        onPress={handleCloseEvent}
-      ></ActiveEventScreen>
-    );
-  }
-  if (closeEvent) {
-    return (
-      <CloseActiveEventScreen
-        onBack={handleCloseEventBack}
-        onClose={handleClosing}
-      ></CloseActiveEventScreen>
-    );
-  }
-
-  return <LoginScreen onPress={handleLogIn}></LoginScreen>;
-  // return <CloseActiveEventScreen></CloseActiveEventScreen>;
+  return <Navigation></Navigation>;
 }
--- Mobile/navigation/Navigation.js
+++ Mobile/navigation/Navigation.js
@@ -1,14 +1,114 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import React, { useState } from "react";
 
 import LoginScreen from "../screens/LoginScreen";
 import WelcomePage from "../screens/WelcomePage";
+import ActiveEventScreen from "../screens/ActiveEventScreen";
+import CloseActiveEventScreen from "../screens/CloseActiveEventScreen";
+import PreviousEvents from "../screens/PreviousEvents";
+import PreviousEventDetails from "../screens/PreviousEventDetails";
 
-const MealsNavigator = createStackNavigator({
-  LoginScreen: LoginScreen,
-  WelcomePage: {
-    screen: WelcomePage,
-  },
-});
+const Navigation = (props) => {
+  const [authorized, setAuthorized] = useState(false);
+  const [prevEvents, setPrevEvents] = useState(false);
+  const [preEventDetails, setPrevEventDetails] = useState(false);
+  const [activeEvent, setActiveEvent] = useState(false);
+  const [closeEvent, setCloseEvent] = useState(false);
+  const [eventId, setEventId] = useState(false);
 
-export default createAppContainer(LoginScreen);
+  const handleLogIn = () => {
+    setAuthorized(true);
+  };
+  const hangleLogOut = () => {
+    setAuthorized(false);
+  };
+  const handlePreveEvent = () => {
+    setPrevEvents(true);
+    setAuthorized(false);
+  };
+  const handlePreveEventBack = () => {
+    setPrevEvents(false);
+    setAuthorized(true);
+  };
+  const handlePreveEventDetails = (id) => {
+    setEventId(id);
+    setPrevEvents(false);
+    setPrevEventDetails(true);
+  };
+  const handlePreveEventDetailsBack = () => {
+    setPrevEvents(true);
+    setPrevEventDetails(false);
+  };
+
+  const handleActiveEvent = () => {
+    setActiveEvent(true);
+    setAuthorized(false);
+  };
+
+  const handleActiveEventBack = () => {
+    setActiveEvent(false);
+    setAuthorized(true);
+  };
+
+  const handleCloseEvent = () => {
+    setCloseEvent(true);
+    setActiveEvent(false);
+  };
+
+  const handleCloseEventBack = () => {
+    setCloseEvent(false);
+    setActiveEvent(true);
+  };
+
+  const handleClosing = () => {
+    setCloseEvent(false);
+    setAuthorized(true);
+  };
+
+  if (authorized) {
+    return (
+      <WelcomePage
+        onPress={hangleLogOut}
+        clickPrev={handlePreveEvent}
+        clickActive={handleActiveEvent}
+      ></WelcomePage>
+    );
+  }
+
+  if (prevEvents) {
+    return (
+      <PreviousEvents
+        onBack={handlePreveEventBack}
+        onPress={handlePreveEventDetails}
+      ></PreviousEvents>
+    );
+  }
+  if (preEventDetails) {
+    return (
+      <PreviousEventDetails
+        onBack={handlePreveEventDetailsBack}
+        id={eventId}
+      ></PreviousEventDetails>
+    );
+  }
+
+  if (activeEvent) {
+    return (
+      <ActiveEventScreen
+        onBack={handleActiveEventBack}
+        onPress={handleCloseEvent}
+      ></ActiveEventScreen>
+    );
+  }
+  if (closeEvent) {
+    return (
+      <CloseActiveEventScreen
+        onBack={handleCloseEventBack}
+        onClose={handleClosing}
+      ></CloseActiveEventScreen>
+    );
+  }
+
+  return <LoginScreen onPress={handleLogIn}></LoginScreen>;
+};
+
+export default Navigation;
